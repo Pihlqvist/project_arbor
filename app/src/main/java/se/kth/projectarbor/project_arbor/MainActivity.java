345ec23e8811850d4c3445a51e7c856a91f75113
pateriks <pateriks@kth.se>
2017-04-20 14:21:16 +0200

    handler boundservice implemented but not tested

--- app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
@@ -1,5 +1,10 @@
 package se.kth.projectarbor.project_arbor;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Methods to bind to MainService
+    boolean isBound;
+    public ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service){
+            MainService.ServiceBinder binder = (MainService.ServiceBinder) service;
+            MainService = binder.getServiceInstance();
+            isBound = true;
+        }
+        public void onServiceDisconnected(ComponentName arg0) {
+            isBound = false;
+        }
+    };
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, MainService.class);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+    protected void onStop() {
+        super.onStop();
+        if (isBound){
+            unbindService(serviceConnection);
+        }
     }
 }
--- app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
@@ -2,15 +2,31 @@ package se.kth.projectarbor.project_arbor;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
 public class MainService extends Service {
+    private final IBinder binder = new ServiceBinder;
+
+    public class ServiceBinder extends Binder {
+        //Provides Service bind methods
+
+        //getServiceInstance provides a instance of the Service
+        MainService getServiceInstance() {
+            return MainService.this;
+        }
+    }
     public MainService() {
     }
-
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    private final class ServiceHandler extends Handler {
+
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
     }
 }
