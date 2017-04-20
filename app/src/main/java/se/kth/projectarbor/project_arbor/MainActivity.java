7c8ac0bdb854f4145c6e53593f489e99aaec5dd9
ramsin93 <ramcin_ou@live.se>
2017-04-20 17:10:30 +0200

    Partial implementation with conflicts between binder and handler

--- app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
@@ -7,35 +7,68 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    MainService mainService;
+
+    Button mStartService;
+    Button mStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, MainService.class);
+        //Bundle bundle = new Bundle();
+        //bundle.putInt("MESSAGE_TYPE", MainService.MSG_START_RUN);
+        //intent.putExtras(bundle);
+        //startService(intent);
+
+        mStartService = (Button) findViewById(R.id.start_service_btn);
+        mStartService.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            }
+        );
+
+        mStopService = (Button) findViewById(R.id.stop_service_btn);
+        mStopService.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            }
+        );
     }
 
     //Methods to bind to MainService
     boolean isBound;
+
     public ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
-        public void onServiceConnected(ComponentName className, IBinder service){
+        public void onServiceConnected(ComponentName className, IBinder service) {
             MainService.ServiceBinder binder = (MainService.ServiceBinder) service;
-            MainService = binder.getServiceInstance();
+            mainService = binder.getServiceInstance();
             isBound = true;
         }
+
+        @Override
         public void onServiceDisconnected(ComponentName arg0) {
             isBound = false;
         }
     };
-    protected void onStart
 
-    {
+    @Override
+    protected void onStart() {
         super.onStart();
         Intent intent = new Intent(this, MainService.class);
-        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
+
+    @Override
     protected void onStop() {
         super.onStop();
         if (isBound){
--- app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
@@ -3,20 +3,44 @@ package se.kth.projectarbor.project_arbor;
 import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.widget.Toast;
 
-public class MainService extends Service {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainService extends Service implements LocationListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private final IBinder binder = new ServiceBinder();
+
+    private ServiceHandler handler;
+    final static int MSG_START_RUN = 0;
+    final static int MSG_STOP_RUN = 1;
+
+    private static long LOCATION_UPDATE_INTERVAL = 20000;
+    private static int ERROR_MARGIN = 5;
+
+    private float mTotalDistance;
+    private Location mCurrentLocation;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
 
     //Binder Service
-
-    private final IBinder binder = new ServiceBinder;
 
     public class ServiceBinder extends Binder {
         //Provides Service bind methods
@@ -26,6 +50,7 @@ public class MainService extends Service {
             return MainService.this;
         }
     }
+
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
@@ -33,26 +58,29 @@ public class MainService extends Service {
 
     // HandlerService
 
-    static final int MSG_START_RUN = 0;
-
     private final class ServiceHandler extends Handler {
-
-        @Override
         public ServiceHandler(Looper looper) {
             super(looper);
         }
+
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_START_RUN:
                     Toast.makeText(getApplicationContext(), "Service is tracking your activity", Toast.LENGTH_SHORT).show();
+                    // start tracking
+                    mGoogleApiClient.connect();
+                case MSG_STOP_RUN:
+                    // stop tracking
+                    LocationServices.FusedLocationApi.removeLocationUpdates(
+                            MainService.this.mGoogleApiClient, MainService.this);
+                    MainService.this.mGoogleApiClient.disconnect();
+                    Toast.makeText(getApplicationContext(), "Service has stopped tracking your activity", Toast.LENGTH_SHORT).show();
                 default:
                     super.handleMessage(msg);
             }
         }
-
     }
-    private ServiceHandler handler;
 
     @Override
     public void onCreate(){
@@ -64,6 +92,16 @@ public class MainService extends Service {
         Looper looper = thread.getLooper();
         handler = new ServiceHandler(looper);
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(LOCATION_UPDATE_INTERVAL);
     }
 
     @Override
@@ -76,8 +114,56 @@ public class MainService extends Service {
         handler.sendMessage(msg);
         return START_STICKY;
     }
-    //MainService
 
-    public MainService() {
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "Connection Success", Toast.LENGTH_LONG).show();
+
+        try {
+            // mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+            // mGoogleApiClient);
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+        } catch (SecurityException ex) {}
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection Failed", Toast.LENGTH_LONG).show();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) { }
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = mCurrentLocation == null ?  location : mCurrentLocation;
+
+        float delta;
+        delta = mCurrentLocation.distanceTo(location);
+
+        /*
+        float[] results = new float[1];
+        Location.distanceBetween(
+                mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(),
+                location.getLatitude(), location.getLongitude(), results);
+        delta = results[0];
+        */
+
+        if (delta > ERROR_MARGIN) {
+            mTotalDistance += delta;
+            Toast.makeText(this, "mTotalDistance == " + mTotalDistance, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "ERROR MARGIN", Toast.LENGTH_LONG).show();
+        }
+
+        mCurrentLocation = location;
     }
 }
