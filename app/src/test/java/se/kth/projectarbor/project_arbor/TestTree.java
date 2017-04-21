8c581e9c386036334ec8bb3c03a0236bc78220c2
Pethrus Gärdborn <gardborn@kth.se>
2017-04-21 09:48:32 +0200

    added changePhase method, change buffer methods, implemented Buffer class, set treephase constants

--- app/src/test/java/se/kth/projectarbor/project_arbor/TestTree.java
+++ app/src/test/java/se/kth/projectarbor/project_arbor/TestTree.java
@@ -1,7 +1,10 @@
 package se.kth.projectarbor.project_arbor;
 
 /**
- * Created by pethrus on 2017-04-20.
+ * Created by pethrus and lovisa on 2017-04-20.
+ *
+ * The tree class is only instantiated once in the beginning of the game lifecycle.
+ * The tree is a passive object. To communicate with the tree, use the interface methods below.
  */
 
 public class TestTree {
@@ -10,8 +13,18 @@ public class TestTree {
     private final int SEED_SUNBUFFER_MAX = 40;
     private final int SEED_HEALTHBUFFER_MAX = 0;
 
+    private final int SPROUT_WATERBUFFER_MAX = 114;
+    private final int SPROUT_SUNBUFFER_MAX = 182;
+    private final int SPROUT_HEALTHBUFFER_MAX = 3;
 
-    private int age;
+    private final int SAPLING_WATERBUFFER_MAX = 472;
+    private final int SAPLING_SUNBUFFER_MAX = 708;
+    private final int SAPLING_HEALTHBUFFER_MAX = 10;
+
+    private final int GROWN_TREE_WATERBUFFER_MAX = 950;
+    private final int GROWN_TREE_SUNBUFFER_MAX = 1186;
+    private final int GROWN_TREE_HEALTHBUFFER_MAX = 23;
+
     private Phase treePhase;
     private Buffer waterBuffer;
     private Buffer sunBuffer;
@@ -34,6 +47,8 @@ public class TestTree {
                 this.max = newMax;
             }
 
+            public void setValue(int newValue) { this.value = newValue };
+
             public void incrValue(int increaseBy){
                 this.value = this.value + increaseBy;
             }
@@ -42,12 +57,81 @@ public class TestTree {
                 this.value = this.value - decreaseBy;
             }
         }
+
     public TestTree(){
         this.age = 0;
         this.treePhase = Phase.SEED;
         this.waterBuffer = new Buffer(SEED_WATERBUFFER_MAX);
         this.sunBuffer = new Buffer(SEED_SUNBUFFER_MAX);
         this.healthBuffer = new Buffer(SEED_HEALTHBUFFER_MAX);
+    }
+
+    public int getWaterLevel(){
+        return waterBuffer.value;
+    }
+
+    public int getSunLevel(){
+        return sunBuffer.value;
+    }
+
+    public int getHealth(){
+        return healthBuffer.value;
+    }
+
+    public void changeWaterBuffer(boolean increase, int amount){
+        if (increase)
+            waterBuffer.incrValue(amount);
+        else
+            waterBuffer.decrValue(amount);
+    }
+
+    public void changeSunBuffer(boolean increase, int amount){
+        if (increase)
+            sunBuffer.incrValue(amount);
+        else
+            sunBuffer.decrValue(amount);
+    }
+
+    public void changeHealthBuffer(boolean increase, int amount){
+        if (increase)
+            healthBuffer.incrValue(amount);
+        else
+            healthBuffer.decrValue(amount);
+    }
+
+    public void changePhase(){
+        switch(this.treePhase) {
+            case SEED:
+                this.treePhase = Phase.SPROUT;
+
+                this.waterBuffer.setMax(SPROUT_WATERBUFFER_MAX);
+                this.waterBuffer.setValue(SPROUT_WATERBUFFER_MAX);
+                this.sunBuffer.setMax(SPROUT_SUNBUFFER_MAX);
+                this.sunBuffer.setValue(SPROUT_SUNBUFFER_MAX);
+                this.healthBuffer.setMax(SPROUT_HEALTHBUFFER_MAX);
+                this.healthBuffer.setValue(SPROUT_HEALTHBUFFER_MAX);
+                break;
+            case SPROUT:
+                this.treePhase = Phase.SAPLING;
+
+                this.waterBuffer.setMax(SAPLING_WATERBUFFER_MAX);
+                this.waterBuffer.setValue(SAPLING_WATERBUFFER_MAX);
+                this.sunBuffer.setMax(SAPLING_SUNBUFFER_MAX);
+                this.sunBuffer.setValue(SAPLING_SUNBUFFER_MAX);
+                this.healthBuffer.setMax(SAPLING_HEALTHBUFFER_MAX);
+                this.healthBuffer.setValue(SAPLING_HEALTHBUFFER_MAX);
+                break;
+            case SAPLING:
+                this.treePhase = Phase.GROWN_TREE;
+
+                this.waterBuffer.setMax(GROWN_TREE_WATERBUFFER_MAX);
+                this.waterBuffer.setValue(GROWN_TREE_WATERBUFFER_MAX);
+                this.sunBuffer.setMax(GROWN_TREE_SUNBUFFER_MAX);
+                this.sunBuffer.setValue(GROWN_TREE_SUNBUFFER_MAX);
+                this.healthBuffer.setMax(GROWN_TREE_HEALTHBUFFER_MAX);
+                this.healthBuffer.setValue(GROWN_TREE_HEALTHBUFFER_MAX);
+                break;
+        }
     }
 
 }
