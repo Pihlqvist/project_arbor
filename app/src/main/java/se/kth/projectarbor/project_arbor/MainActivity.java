256629f99b2b129a996f593ab70adbda0d5ee2a8
Joseph <jariss93@gmail.com>
2017-04-21 15:06:38 +0200

    Demo UI

--- app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainActivity.java
@@ -1,7 +1,11 @@
 package se.kth.projectarbor.project_arbor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+   public void  goTillTree (View view){
+       Intent intent = new Intent(this, TreeGame.class);
+       startActivity(intent);
+   }
 }
---
+++ app/src/main/java/se/kth/projectarbor/project_arbor/TreeGame.java
@@ -0,0 +1,26 @@
+package se.kth.projectarbor.project_arbor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TreeGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tree_game);
+    }
+
+    public void onClick(View view){
+        Button startButton = (Button) findViewById(R.id.startButton);
+        if(startButton.getText().equals("Start Walking")){
+            startButton.setText("Stop Walking");
+        }
+        else {
+            startButton.setText("Start Walking");
+        }
+    }
+}
