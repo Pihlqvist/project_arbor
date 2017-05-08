4470ca6e6ba90b5dd6270c47ac0947702595247b
ramsin93 <ramcin_ou@live.se>
2017-05-08 08:10:19 +0200

    Possible to add an arbitrary number of raindrops and location of the cloud

--- app/src/main/java/se/kth/projectarbor/project_arbor/RainView.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/RainView.java
@@ -1,22 +1,11 @@
 package se.kth.projectarbor.project_arbor;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
-import android.support.constraint.ConstraintLayout;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Display;
-import android.view.Gravity;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -30,23 +19,18 @@ import java.util.Random;
 public class RainView {
 
     private Random random = new Random();
-    private int low = 1700;
-    private int high = 2000;
 
     private ImageView darkCloudIV;
-    private ImageView drop1;
     private ImageView[] raindropsIV;
+
+    private final int NUM_OF_DROPS = 8;
+    private final int SCREEN_WIDTH_DIVISOR = 3;
+    private final float SCREEN_X_OFFSET_PERCENTAGE = 0.75f;
+    private final float SCREEN_Y_OFFSET_PERCENTAGE = 0.10f;
 
     private RelativeLayout.LayoutParams layoutParams;
 
-
     public RainView(Activity activity) {
-        darkCloudIV = new ImageView(activity);
-        darkCloudIV.setImageResource(R.drawable.rain_cloud_1);
-
-        layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-
         Display display = activity.getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
@@ -55,52 +39,50 @@ public class RainView {
         opt.inJustDecodeBounds = true;
         BitmapFactory.decodeResource(activity.getResources(), R.drawable.rain_cloud_1, opt);
 
-        layoutParams.width = size.x/3;
+        raindropsIV = new ImageView[NUM_OF_DROPS];
+        darkCloudIV = new ImageView(activity);
+        darkCloudIV.setImageResource(R.drawable.rain_cloud_1);
+
+        layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.width = size.x/SCREEN_WIDTH_DIVISOR;
         layoutParams.height = layoutParams.width * opt.outHeight / opt.outWidth;
-
-        layoutParams.leftMargin = (int) (0.75f * size.x - layoutParams.width/2);
-        layoutParams.topMargin = (int) (0.10f * size.y - layoutParams.height/2);
-
-        int rand = random.nextInt(high-low) + low;
-        TranslateAnimation translate1 =
-                new TranslateAnimation(0.75f * size.x, 0.75f * size.x, 0.10f * size.y, 0.90f * size.y);
-        translate1.setDuration(rand);
-        translate1.setRepeatMode(Animation.RESTART);
-        translate1.setRepeatCount(Animation.INFINITE);
-        AlphaAnimation alpha1 = new AlphaAnimation(1f, 0.4f);
-        alpha1.setDuration(rand);
-        alpha1.setRepeatMode(Animation.REVERSE); // Originally, REVERSE
-        alpha1.setRepeatCount(Animation.INFINITE);
-        AnimationSet animationSet = new AnimationSet(false);
-        animationSet.addAnimation(translate1);
-        //animationSet.addAnimation(alpha1);
-        drop1 = new ImageView(activity);
-        drop1.setImageResource(R.drawable.rain_drop);
-
+        layoutParams.leftMargin = (int) (SCREEN_X_OFFSET_PERCENTAGE * size.x - layoutParams.width/2);
+        layoutParams.topMargin = (int) (SCREEN_Y_OFFSET_PERCENTAGE * size.y - layoutParams.height/2);
         darkCloudIV.setLayoutParams(layoutParams);
-        drop1.startAnimation(animationSet);
 
-        // Animation translate_left = AnimationUtils.loadAnimation(activity, R.anim.translate_cloud);
-        // darkCloudIV.startAnimation(translate_left);
+        for (int i = 0; i < NUM_OF_DROPS; i++) {
+            ImageView drop = new ImageView(activity);
+            drop.setImageResource(R.drawable.rain_drop);
+            raindropsIV[i] = drop;
 
-        /*float scaleRatio = context.getResources().getDisplayMetrics().density;
-
-
-        Float scale = 0.75f;
-        darkCloudIV.setScaleX(scale);
-        darkCloudIV.setScaleY(scale);
-
-        int dps = 100;
-        int pixle = (int) (dps * scaleRatio + 0.5f);
-        darkCloudIV.setX(pixle);*/
-
-
+            Animation animation = getDropAnimation((int) (SCREEN_X_OFFSET_PERCENTAGE * size.x) + random.nextInt(layoutParams.width) - layoutParams.width/2,
+                    (int) (SCREEN_Y_OFFSET_PERCENTAGE * size.y), size.y);
+            drop.startAnimation(animation);
+        }
     }
 
+    private Animation getDropAnimation(int x, int fromY, int toY) {
+        final int DURATION_LOWER_BOUND = 1700;
+        final int DURATION_HIGH_BOUND = 2000;
+        int rand = random.nextInt(DURATION_HIGH_BOUND - DURATION_LOWER_BOUND) + DURATION_LOWER_BOUND;
+        TranslateAnimation translate =
+                new TranslateAnimation(x, x, fromY, toY);
+        translate.setDuration(rand);
+        translate.setRepeatMode(Animation.RESTART);
+        translate.setRepeatCount(Animation.INFINITE);
+
+        return translate;
+    }
 
     public ViewGroup addViews(ViewGroup layout) {
-        layout.addView(drop1);
+        for (ImageView view : raindropsIV) {
+            layout.addView(view);
+        }
+
         layout.addView(darkCloudIV);
+
         return layout;
     }
 }
