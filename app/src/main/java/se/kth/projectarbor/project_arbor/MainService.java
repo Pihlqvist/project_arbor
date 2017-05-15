e8ab659bcdf3a85fc543ffd5ee8e1304a4698cd4
Pihlqvist <fpih@kth.se>
2017-05-15 14:52:33 +0200

    adding user input in main service

--- app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/MainService.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -47,6 +48,7 @@ public class MainService extends Service {
     public final static int MSG_RESUME_HEAVY = 9;
     public final static int MSG_RESUME_LIGHT = 10;
     public final static int MAIN_FOREGROUND = 111;
+    public final static int MSG_USER_INPUT = 42;
 
     // MainService works with following components
     private Pedometer pedometer;
@@ -57,6 +59,7 @@ public class MainService extends Service {
     private double totalDistance; //distance to be stored in file and handled in mainservice
     private Environment.Weather lastWeather;
     private double lastTemperature;
+    private SharedPreferences sharedPreferences;
     // end
 
     // User information  // TODO: the user should change these thyself  (Fredrik)
@@ -74,6 +77,9 @@ public class MainService extends Service {
     @Override
     public void onCreate() {
         Log.d(TAG, "Service onCreate()");
+
+        sharedPreferences = getSharedPreferences("se.kth.projectarbor.project_arbor", MODE_PRIVATE);
+        readUserSettings();
 
         // Load essential information from IO
         List<Object> list = DataManager.readState(this, filename);
@@ -191,6 +197,14 @@ public class MainService extends Service {
 
                 sendWeatherToView(pendingIntent);
                 break;
+
+            // User have changed settings, make the live
+            case MSG_USER_INPUT:
+                readUserSettings();
+                pedometer.setGender(userGender);
+                pedometer.setHeight(userLength);
+                break;
+
         }
         return START_NOT_STICKY;
     }
@@ -333,6 +347,15 @@ public class MainService extends Service {
 
     private void sendWeatherToView(final PendingIntent pendingIntent) {
         new AsyncTaskRunner().execute(pendingIntent);
+    }
+
+    private void readUserSettings() {
+        if (sharedPreferences.contains("USER_GENDER")) {
+            userGender = Pedometer.Gender.fromString(sharedPreferences.getString("USER_GENDER", "Female"));
+        } else { Log.e(TAG, "user_gender was not found"); }
+        if (sharedPreferences.contains("USER_HEIGHT")) {
+            userLength = sharedPreferences.getFloat("USER_HEIGHT", 1.5f);
+        } else { Log.e(TAG, "user_height was not found"); }
     }
 }
 
