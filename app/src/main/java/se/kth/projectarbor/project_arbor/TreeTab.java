e283d08c7500095c256fc8a49f30a705e19c208a
lovisajosefsson <ljose@kth.se>
2017-05-17 09:53:44 +0200

    moved tree

--- app/src/main/java/se/kth/projectarbor/project_arbor/TreeTab.java
+++ app/src/main/java/se/kth/projectarbor/project_arbor/TreeTab.java
@@ -189,7 +189,7 @@ public class TreeTab extends Fragment {
         Point size = new Point();
         display.getSize(size);
         RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        lp.setMargins(((size.x/100)*10), size.y/2-20*(size.y/100), 10*(size.x/100), 28*(size.y/100));//left, top, right, bottom
+        lp.setMargins(((size.x/100)*10), size.y/2-20*(size.y/100), 10*(size.x/100), 32*(size.y/100));//left, top, right, bottom
         currentLayout.addView(treeAnimView, lp);
 
         RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
